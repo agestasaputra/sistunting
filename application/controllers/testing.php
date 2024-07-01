2b0cde3b7ad64127890a2309224c215304a5fac9
Agesta Saputra <agesta.budy@mekari.com>
2024-07-01 13:04:27 +0700

    chore: set a description hint in bahasa

--- application/controllers/testing.php
+++ application/controllers/testing.php
@@ -18,7 +18,7 @@ class testing extends CI_Controller
             Ups..Silahkan Login Terlebih Dahulu yah, Terima kasih!!</div>');
             redirect('auth');
         }
-        $data['judul'] = 'Tetsing Stunting';
+        $data['judul'] = 'Testing Stunting';
         $data['nama'] =  $this->session->userdata('nama');
         $data['data_gejala'] = $this->model_data_gejala->get();
         $data['data_balita'] = $this->model_data_balita->get();
@@ -40,64 +40,85 @@ class testing extends CI_Controller
     // NEW LOGIC
     public function proses()
     {
-        // function for calculate CF
-        function cf_formula($cf_current, $cf_next)
+        // fungsi ini bertujuan untuk menghitung nilai CF per gejala
+        function cf_formula($cf_variable_satu, $cf_variable_dua)
         {
-            $formula = $cf_current + $cf_next * (1 - $cf_current);
+            // kode ini adalah formula/rumus untuk menghitung nilai CF pergejala
+            $formula = $cf_variable_satu + $cf_variable_dua * (1 - $cf_variable_satu);
+            // round_value bertujuan untuk membulatkan nilai CF pergejala menjadi 3 angka dibelakang koma
             $round_value = round($formula, 3);
             return $round_value;
         }
+        // fungsi ini adalah fungsi utama untuk memproses perhitungan nilai CF 
+        function logic_proses_calculate_cf_percentage($inputan_nilai_cf)
+        {
+            $cf_old = 0;
+            $cf_percentage = 0;
+            
+            // $inputan_nilai_cf_filter adalah value $inputan_nilai_cf yang lebih besar dari 0
+            $inputan_nilai_cf_filter = array_values(array_filter($inputan_nilai_cf, function ($value) {
+                return $value > 0;
+            }));
+            
+            // jika $inputan_nilai_cf_filter kosong, maka $cf_percentage = 0
+            if (count($inputan_nilai_cf_filter) === 0) {
+                print_r('masuk 1');
+                print_r('<br>');
+                $cf_percentage = 0;
+            }
 
+            // jika $inputan_nilai_cf_filter hanya memiliki 1 value, maka $cf_percentage = $inputan_nilai_cf_filter[0] * 100
+            if (count($inputan_nilai_cf_filter) === 1) {
+                print_r('masuk 2');
+                print_r('<br>');
+                $cf_percentage = $inputan_nilai_cf_filter[0] * 100;
+            }
+
+            // jika $inputan_nilai_cf_filter memiliki lebih dari 1 value, maka akan menghasilkan $cf_old dan $cf_percentage dengan menggunakan $cf_formula
+            if (count($inputan_nilai_cf_filter) > 1) {
+                print_r('masuk 3');
+                print_r('<br>');
+
+                // menghasilkan $cf_old dengan menggunakan $cf_formula dari $inputan_nilai_cf_filter[0] dan $inputan_nilai_cf_filter[1]
+                $cf_old = cf_formula($inputan_nilai_cf_filter[0], $inputan_nilai_cf_filter[1]);
+
+                // jika $inputan_nilai_cf_filter memiliki lebih dari 2 value, maka akan dilakukan looping
+                if (count($inputan_nilai_cf_filter) > 2) {
+                    foreach ($inputan_nilai_cf_filter as $index => $pilihan) {
+                        // $cf adalah nilai dari $inputan_nilai_cf_filter selanjutnya
+                        $cf_next = $inputan_nilai_cf_filter[$index];
+
+                        // jika $index lebih dari 1, maka $cf_old akan ditimpa dengan nilai baru hasil dari $cf_formula
+                        if ($index > 1) {
+                            $cf_old = cf_formula($cf_old, $cf_next);
+                        }
+                    }
+                }
+                
+                // $cf_percentage adalah nilai dari $cf_old dikali 100
+                $cf_percentage = $cf_old * 100;
+            }
+            return $cf_percentage;
+        }
+
+        
+        // mengambil nama_balita dari halaman view dan menyimpannya ke dalam variabel
         $id_balita = $this->input->post('nama_balita');
-
-        // Mengambil data detail balita dari db
+        // Mengambil data detail balita dari db dan menyimpannya ke dalam variabel
         $data_detail_balita = $this->model_data_balita->get_detail($id_balita[0]);
 
-        //mengambil inputan data gejala dari pengguna
+        // mengambil value dari seluruh form dari halaman view dan menyimpannya ke dalam variabel
         $nama_balita = $data_detail_balita['nama_balita'];
         $jenis_kelamin = $this->input->post('jenis_kelamin');
         $usia = $this->input->post('usia');
         $tinggi_badan = $this->input->post('tinggi_badan');
         $inputan_gejala = $this->input->post('pilihan');
         $inputan_nilai_cf = $this->input->post('nilai_cf_calculation');
-        
-        // variable for calculate CF
-        $cf_old = [];
-        $cf_percentage = 0;
-            
-        $inputan_nilai_cf_filter = array_values(array_filter($inputan_nilai_cf, function ($value) {
-            return $value > 0;
-        }));
-        
-        // if $inputan_nilai_cf_filter is empty, will set $cf_percentage to 0
-        if (count($inputan_nilai_cf_filter) === 0) {
-            print_r('masuk 1');
-            print_r('<br>');
-            $cf_percentage = 0;
-        }
-        // if $inputan_nilai_cf_filter has 1 value, will set $cf_percentage = $inputan_nilai_cf_filter[0] * 100
-        if (count($inputan_nilai_cf_filter) === 1) {
-            print_r('masuk 2');
-            print_r('<br>');
-            $cf_percentage = $inputan_nilai_cf_filter[0] * 100;
-        }
-        // if $inputan_nilai_cf_filter has more than 1 value, will generate $cf_old and $cf_percentage with $cf_formula
-        if (count($inputan_nilai_cf_filter) > 1) {
-            print_r('masuk 3');
-            print_r('<br>');
-            $cf_old[] = cf_formula($inputan_nilai_cf_filter[0], $inputan_nilai_cf_filter[1]);
-            if (count($inputan_nilai_cf_filter) > 2) {
-                foreach ($inputan_nilai_cf_filter as $index => $pilihan) {
-                    $cf = $inputan_nilai_cf_filter[$index];
-                    if ($index > 1) {
-                        $cf_old[] = cf_formula($cf_old[count($cf_old) - 1], $cf);
-                    }
-                }
-            }
-            $cf_percentage = $cf_old[count($cf_old) - 1] * 100;
-        }
 
-        // collect data to be inserted into table_data_konsultasi
+        // logic untuk memproses kalkulasi dari nilai CF per gejala
+        $cf_percentage = logic_proses_calculate_cf_percentage($inputan_nilai_cf);
+
+        // mengumpulkan data yang akan dimasukkan ke dalam table_data_konsultasi
         $data = [
             'nama_balita' => $nama_balita,
             'jenis_kelamin' => $jenis_kelamin,
@@ -105,12 +126,12 @@ class testing extends CI_Controller
             'tinggi' => $tinggi_badan,
             'hasil' => $cf_percentage
         ];
-        // insert data to table_data_konsultasi
+        // insert data ke table_data_konsultasi
         $this->db->insert('table_data_konsultasi', $data);
-        // toast flashdata
+        // notifikasi flashdata
         $this->session->set_flashdata('notif', '<div class="alert alert-success" role="alert">
         <b>Data Konsultasi Berhasil Ditambahkan! </div>');
-        // redirect into page '/data_konsultasi'
+        // auto pindah halaman ke url '/data_konsultasi'
         redirect('data_konsultasi');
         return;
     }
--- application/views/testing/index.php
+++ application/views/testing/index.php
@@ -18,7 +18,7 @@
               <div class="form-group row mb-3 mx-2">
                 <div class="col-sm-12 col-md-12">
                   <h6 class="text-dark">Nama Balita</h6>
-                  <select name="nama_balita[]" id="nama_balita" class="form-control" onchange="handleDropdownChange()">
+                  <select name="nama_balita[]" id="select_nama_balita" class="form-control" onchange="handle_dropdown_change(this.options[this.selectedIndex].value)">
                     <option value=""> Pilih nama balita</option>
                     <?php
                     $no = 1;
@@ -36,7 +36,7 @@
               <div class="form-group row mb-3 mx-2">
                 <div class="col-sm-12 col-md-12">
                   <h6 class="text-dark">Jenis Kelamin (L/P)</h6>
-                  <select name="jenis_kelamin" id="jenis_kelamin" class="form-control" readonly style="pointer-events: none;">
+                  <select name="jenis_kelamin" id="select_jenis_kelamin" class="form-control" readonly style="pointer-events: none;">
                     <option value="">Pilih jenis kelamin</option>
                     <option value="laki-laki"> Laki-laki </option>
                     <option value="perempuan"> Perempuan </option>
@@ -48,7 +48,7 @@
               <div class="form-group row mb-3 mx-2">
                 <div class="col-sm-12 col-md-12">
                   <h6 class="text-dark">Usia (Bulan)<h6>
-                  <input type="number" name="usia" id="usia" class="form-control" placeholder="Masukan usia" required readonly style="pointer-events: none;">
+                  <input type="number" name="usia" id="input_usia" class="form-control" placeholder="Masukan usia" required readonly style="pointer-events: none;">
                 </div>
                 <!-- <input type="hidden" name="kategori_usia" id="kategori_usia" class="form-control" > -->
               </div>
@@ -57,7 +57,7 @@
               <div class="form-group row mb-3 mx-2">
                 <div class="col-sm-12 col-md-12">
                   <h6 class="text-dark">Tinggi Badan Lahir (Cm) </h6>
-                  <input type="number" name="tinggi_badan" id="tinggi_badan" class="form-control" placeholder="Masukan tinggi Badan" required readonly style="pointer-events: none;">
+                  <input type="number" name="tinggi_badan" id="input_tinggi_badan" class="form-control" placeholder="Masukan tinggi Badan" required readonly style="pointer-events: none;">
                 </div>
               </div>
 
@@ -81,11 +81,11 @@
                             <td>
                               <span><?= $a['nama_gejala']; ?></span>
                               <?php
-                                if ($no-1 === 10) echo '<span id="additional-info"><i></i></span>';
+                                if ($no-1 === 10) echo '<span id="text_info_tambahan"><i></i></span>';
                               ?>
                             </td>
                             <td>
-                              <select name="pilihan[]" id="gejala_<?= $no-1 ?>" class="form-control" onchange="handleCf(this.options[this.selectedIndex].text, '<?= $a['nilai_cf'] ?>', '<?= $no-1 ?>')" required <?php echo $no-1 === 10 ? "readonly style='pointer-events: none;'" : ''; ?>>
+                              <select name="pilihan[]" id="select_gejala_<?= $no-1 ?>" class="form-control" onchange="handle_cf(this.options[this.selectedIndex].value, '<?= $a['nilai_cf'] ?>', '<?= $no-1 ?>')" required <?php echo $no-1 === 10 ? "readonly style='pointer-events: none;'" : ''; ?>>
                                 <option value="">Silakan Pilih </option>
                                 <option value="Ya"> Ya</option>
                                 <option value="Tidak"> Tidak</option>
@@ -96,12 +96,12 @@
                                 <input name="kode_gejala[]" class="form-control" value="<?= $a['kode_gejala']; ?>" required>
                               </div>
                             </div>
-                            <div class="row mb-3 mx-2 d-none">
+                            <div class="row mb-3 mx-2">
                               <div class="col-sm-12 col-md-6">
-                                <input name="nilai_cf[]" id="nilai_cf_<?= $no-1 ?>" class="form-control border border-secondary mr-1 mb-1" value="<?= $a['nilai_cf']; ?>" required>
+                                <input name="nilai_cf[]" id="input_nilai_cf_<?= $no-1 ?>" class="form-control border border-secondary mr-1 mb-1" value="<?= $a['nilai_cf']; ?>" required>
                               </div>
                               <div class="col-sm-12 col-md-6">
-                                <input name="nilai_cf_calculation[]" id="nilai_cf_calculation_<?= $no-1 ?>" class="form-control border border-success" value="" required>
+                                <input name="nilai_cf_calculation[]" id="input_nilai_cf_calculation_<?= $no-1 ?>" class="form-control border border-success" value="" required>
                               </div>
                             </div>
                           </tr>
@@ -129,111 +129,128 @@
 </div>
 
 <script type="text/javascript">
-  // this code will run every page is loaded
+  // kode ini akan berjalan setiap kali halaman di load
   window.addEventListener("pageshow", () => {
-    resetInput()
-    // reset nama balita form value
-    const formNamaBalita = document.getElementById('nama_balita');
-    formNamaBalita.value = '';
+    handle_reset_input()
+
+    // reset select_nama_balita value menjadi value kosong
+    const select_nama_balita = document.getElementById('select_nama_balita');
+    select_nama_balita.value = '';
   });
-  // this function purpose is to reset all input value
-  function resetInput() {
-    const formJenisKelamin = document.getElementById('jenis_kelamin');
-    const formTinggiBadan = document.getElementById('tinggi_badan');
-    const formUsia = document.getElementById('usia');
-    const formGelaja1 = document.getElementById('gejala_1');
-    const formGelaja2 = document.getElementById('gejala_2');
-    const formGelaja3= document.getElementById('gejala_3');
-    const formGelaja4 = document.getElementById('gejala_4');
-    const formGelaja5 = document.getElementById('gejala_5');
-    const formGelaja6 = document.getElementById('gejala_6');
-    const formGelaja7 = document.getElementById('gejala_7');
-    const formGelaja8 = document.getElementById('gejala_8');
-    const formGelaja9 = document.getElementById('gejala_9');
-    const formGelaja10 = document.getElementById('gejala_10');
-    formJenisKelamin.value = '';
-    formTinggiBadan.value = '';
-    formUsia.value = '';
-    formGelaja1.value = ''
-    formGelaja2.value = ''
-    formGelaja3.value = ''
-    formGelaja4.value = ''
-    formGelaja5.value = ''
-    formGelaja6.value = ''
-    formGelaja7.value = ''
-    formGelaja8.value = ''
-    formGelaja9.value = ''
-    formGelaja10.value = ''
+
+  // fungsi ini bertujuan untuk mereset semua input atau select form
+  function handle_reset_input() {
+    const select_jenis_kelamin = document.getElementById('select_jenis_kelamin');
+    const input_usia = document.getElementById('input_usia');
+    const input_tinggi_badan = document.getElementById('input_tinggi_badan');
+    const select_gejala_1 = document.getElementById('select_gejala_1');
+    const select_gejala_2 = document.getElementById('select_gejala_2');
+    const select_gejala_3= document.getElementById('select_gejala_3');
+    const select_gejala_4 = document.getElementById('select_gejala_4');
+    const select_gejala_5 = document.getElementById('select_gejala_5');
+    const select_gejala_6 = document.getElementById('select_gejala_6');
+    const select_gejala_7 = document.getElementById('select_gejala_7');
+    const select_gejala_8 = document.getElementById('select_gejala_8');
+    const select_gejala_9 = document.getElementById('select_gejala_9');
+    const select_gejala_10 = document.getElementById('select_gejala_10');
+    select_jenis_kelamin.value = '';
+    input_usia.value = '';
+    input_tinggi_badan.value = '';
+    select_gejala_1.value = ''
+    select_gejala_2.value = ''
+    select_gejala_3.value = ''
+    select_gejala_4.value = ''
+    select_gejala_5.value = ''
+    select_gejala_6.value = ''
+    select_gejala_7.value = ''
+    select_gejala_8.value = ''
+    select_gejala_9.value = ''
+    select_gejala_10.value = ''
   }
-  // this function purpose is to fill the whole form nilai_cf_calculation (exclude nilai_cf_calculation_10) based on gejala value (Ya/Tidak)
-  function handleCf(value, nilaiCf, index) {
+
+  // fungsi ini bertujuan untuk menghandle ketika terdapat perubahan value pada select_gejala_1 sampai select_gejala_9
+  function handle_cf(value_select_gejala, nilai_cf, index) {
     let result = null
-    if (value === 'Ya') {
-      result = +nilaiCf
+    // kode ini bertujuan untuk memberikan value pada variabel result
+    if (value_select_gejala === 'Ya') {
+      // jika value_select_gejala adalah 'Ya', maka result akan diisi dengan nilai_cf
+      result = +nilai_cf
     } else {
+      // jika value_select_gejala adalah 'Tidak', maka result akan diisi dengan 0
       result = 0
     }
-    const domNilaiCf = document.getElementById(`nilai_cf_calculation_${index}`)
-    domNilaiCf.value = result
+    // kode ini bertujuan untuk memberikan value pada input_nilai_cf_calculation_1 sampai input_nilai_cf_calculation_2 berdasarkan result
+    const input_nilai_cf_calculation = document.getElementById(`input_nilai_cf_calculation_${index}`)
+    input_nilai_cf_calculation.value = result
   }
-  function generateDescriptionAge(age, currentHeight) {
-    let heightLimit = 99
-    if (age < 60 ) {
-      heightLimit = 99
-    } if (age < 48) {
-      heightLimit = 87
-    } if (age < 36) {
-      heightLimit = 80
-    } if (age < 24) {
-      heightLimit = 68
-    } if (age < 12) {
-      heightLimit = 45
-    }
-    // generate additional info (height limit) based on age
-    const additionalInfoDOM = document.getElementById('additional-info')
-    additionalInfoDOM.innerHTML = `<i>(Tinggi badan normal untuk usia <b>${age} bulan</b> adalah <b>${heightLimit} cm</b>)</i>`
 
-    // assign value form gelaja_10 based on heightLimit
-    const formGelaja10 = document.getElementById('gejala_10');
-    formGelaja10.value = currentHeight >= heightLimit ? 'Ya' : 'Tidak';
+  // fungsi ini bertujuan untuk membuat deskripsi tambahan berdasarkan usia
+  function generate_description_age(usia, tb_lahir) {
+    let batas_tinggi_badan = 99
+    if (usia < 60 ) {
+      batas_tinggi_badan = 99
+    } if (usia < 48) {
+      batas_tinggi_badan = 87
+    } if (usia < 36) {
+      batas_tinggi_badan = 80
+    } if (usia < 24) {
+      batas_tinggi_badan = 68
+    } if (usia < 12) {
+      batas_tinggi_badan = 45
+    }
+
+    // memberi value text_info_tambahan berdasarkan usia dan batas_tinggi_badan untuk ditampikan di tabel gejala
+    const text_info_tambahan = document.getElementById('text_info_tambahan')
+    text_info_tambahan.innerHTML = `<i>(Tinggi badan normal untuk usia <b>${usia} bulan</b> adalah <b>${batas_tinggi_badan} cm</b>)</i>`
+
+    // memberi value select_gejala_10 berdasarkan tb_lahir dan batas_tinggi_badan 
+    const select_gejala_10 = document.getElementById('select_gejala_10');
+    select_gejala_10.value = tb_lahir >= batas_tinggi_badan ? 'Ya' : 'Tidak';
     
-    // assign value form nilai_cf_10 based on formGelaja10
-    const formNilaiCfCalculation10 = document.getElementById('nilai_cf_calculation_10');
-    const formNilaiCf10 = document.getElementById('nilai_cf_10');
-    formNilaiCfCalculation10.value = formGelaja10.value === 'Ya' ? formNilaiCf10.value : 0
+    // memberi value input_nilai_cf_calculation_10 berdasarkan select_gejala_10 dan input_nilai_cf_10 
+    const input_nilai_cf_calculation_10 = document.getElementById('input_nilai_cf_calculation_10');
+    const input_nilai_cf_10 = document.getElementById('input_nilai_cf_10');
+    input_nilai_cf_calculation_10.value = select_gejala_10.value === 'Ya' ? input_nilai_cf_10.value : 0
   }
-  // this function purpose is to get age based on tglLahir
-  function getAge(tglLahir) {
-    const birthDate = new Date(tglLahir);
+
+  // fungsi ini bertujuan untuk mendapatkan usia berdasarkan tgl_lahir (dd/mm/yyyy)
+  function get_age(tgl_lahir) {
+    // kode ini bertujuan untuk merubah format tgl_lahir menjadi format Date di Javascript
+    const birth_date = new Date(tgl_lahir);
+    // kode ini bertujuan untuk mendapatkan tanggal hari ini (dalam bentuk format Date di Javascript)
     const today = new Date();
-    const monthDiff = (today.getFullYear() - birthDate.getFullYear()) * 12 + (today.getMonth() - birthDate.getMonth());
-    const ageInMonths = monthDiff + (today.getDate() < birthDate.getDate() ? -1 : 0);
-    return ageInMonths
+    // kode ini bertujuan untuk menghitung selisih bulan antara tgl_lahir dan hari ini
+    const month_diff = (today.getFullYear() - birth_date.getFullYear()) * 12 + (today.getMonth() - birth_date.getMonth());
+    // kode ini bertujuan untuk menghitung usia dalam bulan
+    const age_in_months = month_diff + (today.getDate() < birth_date.getDate() ? -1 : 0);
+    return age_in_months
   }
-  // this function purpose is to handle dropdown change event
-  function handleDropdownChange($event) {
+
+  // fungsi ini bertujuan untuk menghandle ketika terdapat perubahan value pada select_nama_balita
+  function handle_dropdown_change(balita_id) {
+    // kode bertujuan untuk melakukan ajax proses untuk mengambil data_detail_balita berdasarkan balita_id
     const BASE_URL = '<?php echo base_url(); ?>';
     $.ajax({
       type: "GET",
-      url: `${BASE_URL}/testing/get_detail_balita/${+event.target.value}`,
+      url: `${BASE_URL}/testing/get_detail_balita/${+balita_id}`,
       dataType: "json",
       success: function(response) {
         console.log('response:', response)
-        // if response is null, reset input and stop the flow
+        // jika response null (kosong/tidak ada datanya), maka reset input dan hentikan alur
         if (!response) {
           resetInput()
           return;
         }
 
-        // fill form value based on response
-        const formJenisKelamin = document.getElementById('jenis_kelamin');
-        formJenisKelamin.value = response.jenis_kelamin;
-        const formTinggiBadan = document.getElementById('tinggi_badan');
-        formTinggiBadan.value = response.tb_lahir;        
-        const formUsia = document.getElementById('usia');
-        const resultAge = getAge(response.tgl_lahir); 
-        formUsia.value = resultAge;
-        generateDescriptionAge(resultAge, response.tb_lahir);
+        // kode ini bertujuan untuk mengisi value select_jenis_kelamin, input_tinggi_badan, dan input_usia berdasarkan response
+        const select_jenis_kelamin = document.getElementById('select_jenis_kelamin');
+        select_jenis_kelamin.value = response.jenis_kelamin;
+        const input_tinggi_badan = document.getElementById('input_tinggi_badan');
+        input_tinggi_badan.value = response.tb_lahir;        
+        const input_usia = document.getElementById('input_usia');
+        const hasil_usia = get_age(response.tgl_lahir); 
+        input_usia.value = hasil_usia;
+        generate_description_age(hasil_usia, response.tb_lahir);
       },
       error: function(xhr, ajaxOptions, thrownError) {
         console.log('error', xhr, ajaxOptions, thrownError)}
